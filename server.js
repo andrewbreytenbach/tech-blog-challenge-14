5dbea0238aaed2e9db5e37e13681ab5044a94965
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-07 09:00:42 -0400

    updated the server.js file in the root directory

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const routes = require('./controllers');
+
+// Create an instance of Express.js
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Set up session middleware
+app.use(
+  session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Set up Handlebars.js as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Middleware to parse JSON and URL-encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
