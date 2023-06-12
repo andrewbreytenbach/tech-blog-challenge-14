0cf35a2776396ea723ccddb5c6d7de8e6f1f4ed8
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-06-12 10:04:23 -0400

    added some js files and then ensured the corresponding handlebars files are linked to the js code

---
+++ public/js/signup.js
@@ -0,0 +1,32 @@
+// signup.js
+
+// Function to handle signup form submission
+const handleSignup = async (event) => {
+    event.preventDefault();
+  
+    // Retrieve user input from the signup form
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (username && email && password) {
+      // Send a POST request to the signup route
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If signup is successful, redirect to the dashboard page
+        document.location.replace('/dashboard');
+      } else {
+        // If signup fails, display an error message
+        alert('Failed to sign up. Please try again.');
+      }
+    }
+  };
+  
+  // Add event listener to the signup form submit button
+  document.querySelector('#signup-form').addEventListener('submit', handleSignup);
+  
